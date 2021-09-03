06ad8bbe2a3ed135346d7595398ee9a2793348b0
shobhit-ranjan <shobhit.ranjan03@gmail.com>
2021-09-03 16:51:38 +0530

    WorkShop UC16 DATABASE Adding new value to DB

--- AdressBookUserDay37.java
+++ AdressBookUserDay37.java
@@ -13,14 +13,14 @@ public class userDatatoDB {
 			String insertEmp = "INSERT INTO addressbookday37 (firstName,lastName,address,city,state,zip,phoneNumber,email) values(?,?,?,?,?,?,?,?)";
 			try {
 				PreparedStatement preparedStatement = conn.prepareStatement(insertEmp);
-				preparedStatement.setString(1, "QWE");
-				preparedStatement.setString(2, "OPI");
-				preparedStatement.setString(3, "A 001");
-				preparedStatement.setString(4, "Nodia");
+				preparedStatement.setString(1, "SHO");
+				preparedStatement.setString(2, "RAN");
+				preparedStatement.setString(3, "A 999");
+				preparedStatement.setString(4, "Varanasi");
 				preparedStatement.setString(5, "UP");
-				preparedStatement.setInt(6, 211015);
-				preparedStatement.setString(7, "8934788982");
-				preparedStatement.setString(8, "qwe@gmail.com");
+				preparedStatement.setInt(6, 211019);
+				preparedStatement.setString(7, "8934788990");
+				preparedStatement.setString(8, "ranh@gmail.com");
 				int rowUpdated = preparedStatement.executeUpdate();
 				if (rowUpdated > 0) {
 					System.out.println("Data is Updated");
