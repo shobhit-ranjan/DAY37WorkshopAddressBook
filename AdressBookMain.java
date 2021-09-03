a18d8208e0100ec6e031534dfc2e7aa8957f6e7b
shobhit-ranjan <shobhit.ranjan03@gmail.com>
2021-09-03 12:32:23 +0530

    WorkShop UC2 File

--- AdressBookMain.java
+++ AdressBookMain.java
@@ -1,19 +1,31 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class AdressBookMain {
 
 	public static void main(String[] args) {
+		AdressBookUserDay37 Entery = new AdressBookUserDay37();
+		// getsqlConnection();
+		// userDatatoDB.insertContact();
+		// userDatatoDB.updateContactNameOfAdressbook();
+		Entery.addContact();
 
-		 getsqlConnection();
-		 userDatatoDB.insertContact();
+		System.out.println("you to make any changes in enter type yes or no");
+		Scanner sc = new Scanner(System.in);
+		String input = sc.next();
+		if (input.equals("yes")) {
+
+			Entery.editContact();
+		}
 	}
+
 	public static Connection getsqlConnection() {
 		Connection conn = null;
 		String hostUrl = "jdbc:mysql://localhost:3306/address_book_service";
 		String userName = "root";
-		String password = "mymaths009!@A";
+		String password = "mymaths00";
 		try {
 			conn = DriverManager.getConnection(hostUrl, userName, password);
 		} catch (SQLException e) {
