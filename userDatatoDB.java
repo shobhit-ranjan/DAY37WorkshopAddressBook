b9d06ef35c1a690c89e5b257575645d9a7e36836
shobhit-ranjan <shobhit.ranjan03@gmail.com>
2021-09-03 14:09:57 +0530

    WorkShop UC16 DATABASE reading

--- userDatatoDB.java
+++ userDatatoDB.java
@@ -1,6 +1,8 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class userDatatoDB {
 	
@@ -34,6 +36,45 @@ public class userDatatoDB {
 						System.out.println(sqlException.getMessage());
 
 					}
+				}
+			}
+		}
+
+	}
+	public static void readAdressbook() {
+		System.out.println("Displaying all data of adressBook table");
+	  Connection conn = AdressBookMain.getsqlConnection();
+
+		try {
+			if (conn != null) {
+				String readEmpPayroll = "SELECT * FROM addressbookday37";
+
+				Statement statement = conn.createStatement();
+				ResultSet resultSet = statement.executeQuery(readEmpPayroll);
+				while (resultSet.next()) {
+					String firstName = resultSet.getString(1);
+					String lastName = resultSet.getString(2);
+					String adress = resultSet.getString(3);
+					String city = resultSet.getString(4);
+					String state = resultSet.getString(5);
+					Integer zip = resultSet.getInt(6);
+					String phoneNumber = resultSet.getString(7);
+					String email = resultSet.getString(8);
+					String row = String.format(
+							"User record:\n FirstName: %s,\n LastName: %s, \n Address: %s,  \n City: %s,\n State: %s , \n Zip: %d, \n PhoneNumber: %s, \n email:%s",
+							firstName, lastName, adress, city, state, zip, phoneNumber, email);
+					System.out.println(row);
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException sqlException) {
+					System.out.println(sqlException.getMessage());
+
 				}
 			}
 		}
